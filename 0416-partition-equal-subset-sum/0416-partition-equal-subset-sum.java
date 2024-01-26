f93494da6040e66065225bf9e9fecee91abd0bf5
komal raut <97026444+Komalraut2002@users.noreply.github.com>
2024-01-26 08:45:12 +0530

    Time: 40 ms (63.97%), Space: 47.2 MB (44.79%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,41 +1,38 @@
 class Solution {
-    boolean helper(int i,int k,int[]nums,int[][]dp){
-        if(i==0){
-            return k == nums[0]?true:false;
-        }
-        if(k==0){
-            return true;
-        }
-        if(dp[i][k] != -1){
-            return dp[i][k] == 0 ? false : true;
-        }
-        
-        boolean notTake=helper(i-1,k,nums,dp);
-        boolean take=false;
-        if(k>=nums[i]){
-            take=helper(i-1,k-nums[i],nums,dp);
-        }
-        dp[i][k]=(notTake||take)? 1 : 0;
 
-        return notTake||take;
-    }
     public boolean canPartition(int[] nums) {
-       
-        int n=nums.length;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
+        int n = nums.length;
+        int sum = 0;
+        int k;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
         }
-        if(sum%2!=0){
+        if (sum % 2 != 0) {
             return false;
         }
-        int k=sum/2;
-        int [][]dp=new int [n+1][k+1];
-        for (int row[] : dp)
-                Arrays.fill(row, -1);
+        else{
+        k = sum / 2;
+        }
+        boolean[][] dp = new boolean[n][k + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        if (nums[0] <= k) {
+            dp[0][nums[0]] = true;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int target = 1; target <= k; target++) {
+                boolean notTake = dp[i-1][target];
+                boolean take = false;
+                if ( nums[i]<=target) {
+                    take = dp[i-1][target - nums[i]];
+                }
+                dp[i][target] = (notTake || take);
+            }
+        }
 
-        return helper(n-1,k,nums,dp);
+        return dp[n - 1][k];
     }
 }
 
-// memoization t.c=O(N*K)+O(N)
+// tabulation t.c=O(N*K)+O(N)
