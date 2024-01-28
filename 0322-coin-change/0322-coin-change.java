550043a17d186bc7767ee6b776834df04c22f224
komal raut <97026444+Komalraut2002@users.noreply.github.com>
2024-01-28 20:41:39 +0530

    Time: 111 ms (9.81%), Space: 44.9 MB (21.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,34 +1,27 @@
 class Solution {
-    
-    static int helper(int i,int amount,int[]coins,int [][]dp){
-        if(i==0){
-            if(amount%coins[0]==0){
-                return amount/coins[0];
-                }
-                else{
-                    return (int) Math.pow(10, 9);
-                }
-        }
-        if(dp[i][amount]!=-1){
-            return dp[i][amount];
-        }
-        int notTake=0+helper(i-1,amount,coins,dp);
-        int take=(int) Math.pow(10, 9);
-        if(coins[i]<=amount){
-            take=1+helper(i,amount-coins[i],coins,dp);
-        }
-        dp[i][amount]= Math.min(take,notTake);
-        return dp[i][amount];
-    }
-    public int coinChange(int[] coins, int amount) {
-        
+    public int coinChange(int[] coins, int amount) { 
         int n=coins.length;
         int [][]dp=new int[n+1][amount+1];
-        for(int []row:dp){
-            Arrays.fill(row,-1);
+        for(int tar=0;tar<=amount;tar++){
+            if(tar%coins[0]==0){
+                dp[0][tar]= tar/coins[0];
+            }
+            else{
+                dp[0][tar]=(int) Math.pow(10, 9);
+            }
+}
+        for(int i=1;i<n;i++){
+            for(int tar=0;tar<=amount;tar++){
+                int notTake=0+dp[i-1][tar];
+                int take=(int) Math.pow(10, 9);
+                  if(coins[i]<=tar){
+                        take=1+dp[i][tar-coins[i]];
+                   }
+                    dp[i][tar]= Math.min(take,notTake);
+               }
         }
-        int ans= helper(n-1,amount,coins,dp);
-        if (ans >= (int) Math.pow(10, 9))
+        int ans=dp[n-1][amount];
+        if (ans >=(int) Math.pow(10, 9))
             return -1;
         return ans;
     }
@@ -38,6 +31,6 @@ class Solution {
 
 // Reason: There are N*T states therefore at max ‘N*T’ new problems will be solved.
 
-// Space Complexity: O(N*T) + O(N)
+// Space Complexity: O(N*T)
 
-// Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*T)).
+// Reason:  2D array ( O(N*T)).
