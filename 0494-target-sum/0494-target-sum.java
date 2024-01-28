fe84b0e1eb5d7b0f679f0a7070e610c404786c79
komal raut <97026444+Komalraut2002@users.noreply.github.com>
2024-01-28 22:57:41 +0530

    Time: 8 ms (73.12%), Space: 44.6 MB (26.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,31 +1,22 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        int n=nums.length;
-        int sum = Arrays.stream(nums).sum();
-        int offset=sum; 
-        int range = 2 * sum + 1;
-        int [][]dp=new int [n+1][range];
-        for(int []row:dp){
-                Arrays.fill(row,-1);
-        }
-        return helper(0,nums,target,dp,offset,offset);
-    }
-    static int helper(int i,int[]nums,int target,int [][] dp,int j,int offset){
-       int n=nums.length;
-       if (i == n) {
-            return (j - offset == target) ? 1 : 0;
-        }
-        if(i>n-1){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int add=helper(i+1,nums,target,dp,j+nums[i],offset);
-        int sub=helper(i+1,nums,target,dp,j-nums[i],offset);
+        public int findTargetSumWays(int[] nums, int target){
+        int total = Arrays.stream(nums).sum(); 
+        int[][] dp = new int[nums.length][2 * total + 1]; 
 
-        dp[i][j]= (add+sub);
-        return dp[i][j];
+       
+        dp[0][nums[0] + total] = 1;
+        dp[0][-nums[0] + total] += 1;
+
+        for(int i = 1; i < nums.length; i++){
+            
+            for(int sum = -total; sum <= total; sum++){
+               
+                if(dp[i - 1][sum + total] > 0){
+                    dp[i][sum + nums[i] + total] += dp[i - 1][sum + total];
+                    dp[i][sum - nums[i] + total] += dp[i - 1][sum + total];
+                }
+            }
+        }
+        return Math.abs(target) > total ? 0 : dp[nums.length - 1][target + total];
     }
 }
-// memoization;
