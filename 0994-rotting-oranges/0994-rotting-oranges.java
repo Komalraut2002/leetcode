722d2c0399d4a4a905f6f03de5b4bc373b201740
komal raut <97026444+Komalraut2002@users.noreply.github.com>
2024-02-04 20:19:02 +0530

    Time: 3 ms (24.83%), Space: 42.8 MB (18.04%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,63 +1,55 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-       int m=grid.length;
-       int n=grid[0].length;
-       int freshcnt=0;
-       int time=0;
-       Queue <pair> q=new LinkedList<>();
-       for(int i=0;i<m;i++){
-           for(int j=0;j<n;j++){
-               if(grid[i][j]==2){
-                   q.add(new pair(i,j));
-               }
-               if(grid[i][j]==1){
-                   freshcnt++;
-               }
-           }
-       }
-       if(freshcnt==0){
-           return 0;
-       } 
-       while(!q.isEmpty()){
-           int size=q.size();
-           int curr=0;
-           while(size!=0){
-           pair p=q.poll();
-           int x1=p.first;
-           int y1=p.second;
-           int[] delx = {1, -1, 0, 0};
-           int[] dely = {0, 0, -1, 1};
-
-           for(int i=0;i<4;i++){
-               int x=x1+delx[i];
-               int y=y1+dely[i];
-               if(x>=0 && x<m && y>=0 && y<n && grid[x][y]==1){
-                   curr++;
-                   grid[x][y]=2;
-                   q.add(new pair(x,y));
-               }
-           }
-           size--;
+        int m=grid.length;
+        int n=grid[0].length;
+        int fresh=0;
+        Queue<pair> q =new LinkedList<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.add(new pair(i,j,0));
+                }
+                if(grid[i][j]==1){
+                    fresh++;
+                }
+            }
         }
-        if(curr!=0){
-            time++;
+        int cnt=0;
+        int time=0;
+        while(!q.isEmpty()){
+            pair p=q.poll();
+            int x=p.first;
+            int y=p.second;
+            int t=p.tm;
+            time=Math.max(t,time);
+            int [] delx={-1,1,0,0};
+            int [] dely={0,0,1,-1};
+            for(int i=0;i<4;i++){
+                int nrow=x+delx[i];
+                int ncol=y+dely[i];
+                if(nrow<m && ncol<n && nrow>=0 && ncol>=0 && grid[nrow][ncol]==1){
+                    cnt++;
+                    grid[nrow][ncol]=2;
+                    q.add(new pair(nrow,ncol,t+1));
+                }
+            }
         }
-    }
-       for(int i=0;i<m;i++){
-           for(int j=0;j<n;j++){
-               if(grid[i][j]==1){
-                   return -1;
-               }
-           }
-       }
-       return time;
+        if(fresh==0){
+            return 0;
+        }
+        if(cnt!=fresh){
+            return -1;
+        }
+        return time;
     }
 }
-class pair {
+class pair{
     int first;
-    int second; 
-    public pair(int first, int second) {
-        this.first = first; 
-        this.second = second; 
+    int second;
+    int tm;
+    public pair(int first,int second,int tm){
+        this.first=first;
+        this.second=second;
+        this.tm=tm;
     }
 }
