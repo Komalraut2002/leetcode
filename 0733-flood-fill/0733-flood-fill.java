a0e668aaa822c5e08c728c84e337f8204392f3e0
komal raut <97026444+Komalraut2002@users.noreply.github.com>
2024-02-04 12:32:56 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (28.95%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -6,25 +6,24 @@ class Solution {
             return image;
         }
         int old=image[sr][sc];
-        boolean[][] visited=new boolean[m][n];
-        if(image[sr][sc]!=color && !visited[sr][sc]){
-            dfs(visited,image,sr,sc,color,old);
+        if(image[sr][sc]!=color){
+            dfs(image,sr,sc,color,old);
         }
 
         return image;
     }
 
-    static void dfs(boolean[][]visited,int[][]image,int i,int j,int color,int old){
+    static void dfs(int[][]image,int i,int j,int color,int old){
         int m=image.length;
         int n=image[0].length;
-        if (i < 0 || i >= m || j < 0 || j >= n || image[i][j] != old || visited[i][j]) {
+        if (i < 0 || i >= m || j < 0 || j >= n || image[i][j] != old) {
             return;
         }
-        visited[i][j]=true;
+        
         image[i][j]=color;
-        dfs(visited,image, i + 1, j,color,old);
-        dfs(visited,image, i - 1, j,color,old);
-        dfs(visited,image, i, j + 1,color,old);
-        dfs(visited,image, i, j - 1,color,old);
+        dfs(image, i + 1, j,color,old);
+        dfs(image, i - 1, j,color,old);
+        dfs(image, i, j + 1,color,old);
+        dfs(image, i, j - 1,color,old);
     }
 }
