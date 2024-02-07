3859be6b9768cdf4d7eddb2cdf5c235dc3618a26
komal raut <97026444+Komalraut2002@users.noreply.github.com>
2024-02-07 21:17:39 +0530

    Time: 6 ms (54.92%), Space: 46.2 MB (5.87%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,62 +1,40 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Stack;
 
 public class Solution {
-    static void dfs(int i, boolean[] visited, HashMap<Integer, ArrayList<Integer>> adj, Stack<Integer> st) {
-        visited[i] = true;
-        for (int neighbor : adj.get(i)) {
-            if (!visited[neighbor]) {
-                dfs(neighbor, visited, adj, st);
-            }
-        }
-        st.push(i);
-    }
 
-      public boolean canFinish(int numCourses, int[][] prerequisites) {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
         HashMap<Integer, ArrayList<Integer>> adj = new HashMap<>();
-        Stack<Integer> st = new Stack<>();
-        boolean[] visited = new boolean[numCourses];
-
+        int [] indegree=new int [numCourses];
+        Queue<Integer> q=new LinkedList<>();
+        int cnt=0;
         for (int i = 0; i < numCourses; i++) {
             adj.put(i, new ArrayList<>());
         }
 
         for (int i = 0; i < prerequisites.length; i++) {
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            adj.get(u).add(v);
+            int dependent = prerequisites[i][0];
+            int subject = prerequisites[i][1];
+            indegree[dependent]++;
+            adj.get(subject).add(dependent);
         }
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                dfs(i, visited, adj, st);
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.add(i);
             }
         }
-
-    Map<Integer, Integer> pos = new HashMap<>();
-     
-    int ind = 0;
-     
-    while (!st.isEmpty()) 
-    {
-        pos.put(st.peek(), ind);
-        ind += 1;
-        st.pop();
-    }
- 
-    for(int i = 0; i < numCourses; i++) 
-    {
-        for(Integer it : adj.get(i))
-        {
-            if (pos.get(i) >= pos.get(it))
-            {
-                return false;
+        while(!q.isEmpty()){
+            int front=q.poll();
+            cnt++;
+            for(int it:adj.get(front)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(it);
+                }
             }
-        }
-    }
- 
-    return true;
-  }
 
+        }
+
+        return cnt==numCourses;
+    }
 }
