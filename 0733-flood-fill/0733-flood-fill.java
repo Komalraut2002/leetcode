0360b131f8ad246be24dbc1782196a89f95fcafe
komal raut <97026444+Komalraut2002@users.noreply.github.com>
2024-02-20 18:05:44 +0530

    Time: 0 ms (100.00%), Space: 44.7 MB (83.49%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m=image.length;
-        int n=image[0].length;
+        int n=image.length;
+        int m=image[0].length;
         if(image[sr][sc]==color){
             return image;
         }
@@ -9,21 +9,20 @@ class Solution {
         if(image[sr][sc]!=color){
             dfs(image,sr,sc,color,old);
         }
-
         return image;
     }
-
     static void dfs(int[][]image,int i,int j,int color,int old){
-        int m=image.length;
+     int m=image.length;
         int n=image[0].length;
         if (i < 0 || i >= m || j < 0 || j >= n || image[i][j] != old) {
             return;
         }
-        
-        image[i][j]=color;
-        dfs(image, i + 1, j,color,old);
-        dfs(image, i - 1, j,color,old);
-        dfs(image, i, j + 1,color,old);
-        dfs(image, i, j - 1,color,old);
+          image[i][j]=color; 
+          dfs(image,i-1,j,color,old);
+          dfs(image,i+1,j,color,old);
+          dfs(image,i,j-1,color,old);
+          dfs(image,i,j+1,color,old);
+          
     }
 }
+//O(m*n)
