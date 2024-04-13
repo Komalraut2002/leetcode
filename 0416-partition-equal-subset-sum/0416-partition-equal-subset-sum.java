eadfa8b8b5aee68ecc0588f0af9a1b8688a5944a
komal raut <97026444+Komalraut2002@users.noreply.github.com>
2024-04-13 19:39:19 +0530

    Time: 90 ms (21.30%), Space: 57.1 MB (25.15%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,38 +1,28 @@
 class Solution {
-
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
-        int sum = 0;
-        int k;
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
+        int n=nums.length;
+        int sum=0;
+        for(int i:nums){
+            sum+=i;
         }
-        if (sum % 2 != 0) {
-            return false;
+        if(sum%2!=0)return false;
+        int k=sum/2;
+        int [][] dp=new int[n][k+1];
+        for(int[]row:dp){
+            Arrays.fill(row,-1);
         }
-        else{
-        k = sum / 2;
+        return check(n-1,k,nums,dp);
+    }
+    public static boolean check(int i,int k,int[]nums,int[][]dp){
+        if(k==0)return true;
+        if(i==0)return nums[i]==k;
+        if(dp[i][k]!=-1)return dp[i][k]==1?true:false;
+        boolean not=check(i-1,k,nums,dp);
+        boolean take=false;
+        if(nums[i]<=k){
+            take=check(i-1,k-nums[i],nums,dp);
         }
-        boolean[][] dp = new boolean[n][k + 1];
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
-        }
-        if (nums[0] <= k) {
-            dp[0][nums[0]] = true;
-        }
-        for (int i = 1; i < n; i++) {
-            for (int target = 1; target <= k; target++) {
-                boolean notTake = dp[i-1][target];
-                boolean take = false;
-                if ( nums[i]<=target) {
-                    take = dp[i-1][target - nums[i]];
-                }
-                dp[i][target] = (notTake || take);
-            }
-        }
-
-        return dp[n - 1][k];
+        dp[i][k]=not||take?1:0;
+        return dp[i][k]==1?true:false;
     }
 }
-
-// tabulation t.c=O(N*K)+O(N)
