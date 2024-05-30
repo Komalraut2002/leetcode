ec7e5cf42eda28c9a19891a1410349c391138f0e
komal raut <97026444+Komalraut2002@users.noreply.github.com>
2024-05-30 14:28:24 +0530

    Time: 1 ms (40.60%), Space: 45 MB (24.39%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,28 +1,26 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n=image.length;
-        int m=image[0].length;
-        if(image[sr][sc]==color){
+        int m=image.length;
+        int n=image[0].length;
+        int old=image[sr][sc];
+        if(old==color){
             return image;
         }
-        int old=image[sr][sc];
-        if(image[sr][sc]!=color){
-            dfs(image,sr,sc,color,old);
-        }
+        dfs(image,sr,sc,color,old);
         return image;
     }
-    static void dfs(int[][]image,int i,int j,int color,int old){
-     int m=image.length;
+    public void dfs(int[][] image, int sr, int sc, int color,int old){
+        int m=image.length;
         int n=image[0].length;
-        if (i < 0 || i >= m || j < 0 || j >= n || image[i][j] != old) {
-            return;
+        image[sr][sc]=color;
+        int[] x={-1,1,0,0};
+        int[] y={0,0,-1,1};
+        for(int i=0;i<4;i++){
+            int newX=x[i]+sr;
+            int newY=y[i]+sc;
+            if(newX>=0 && newX<m && newY>=0 && newY<n && image[newX][newY]==old){
+                dfs(image,newX,newY,color,old);
+            }
         }
-          image[i][j]=color; 
-          dfs(image,i-1,j,color,old);
-          dfs(image,i+1,j,color,old);
-          dfs(image,i,j-1,color,old);
-          dfs(image,i,j+1,color,old);
-          
     }
 }
-//O(m*n)
